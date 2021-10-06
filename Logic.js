0cfe298003365397554154f169b2f89dbf822d47
Peter <phellwege1@gmail.com>
2021-10-06 14:40:18 -0600

    New Commit5

--- Logic.js
+++ Logic.js
@@ -1,9 +1,10 @@
 
-function CalcTip() {
+function CalcTip(billAmt, tipPercent, numOfPeople) {
     var billAmt = document.getElementById("billMe").value;
     var numOfPeople = document.getElementById("numberOfPeople").value;
     var tipPercent = document.getElementById("buttons_form").value;
-    var total = document.getElementById("total").value;
+
+    var total = document.getElementById("total");
         
     if (billAmt === "" || numOfPeople < 1) {
         alert("Can't be zero");
@@ -15,7 +16,7 @@ function CalcTip() {
     var total = (billAmt * tipPercent) / numOfPeople;
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2);
+
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
 }
-
