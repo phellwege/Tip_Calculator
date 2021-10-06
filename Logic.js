0693eaf001ea553d8e26a1e0de960d774768fda0
Peter <phellwege1@gmail.com>
2021-10-06 14:10:51 -0600

    New Commit4

--- Logic.js
+++ Logic.js
@@ -2,21 +2,16 @@
 function CalcTip() {
     var billAmt = document.getElementById("billMe").value;
     var numOfPeople = document.getElementById("numberOfPeople").value;
-    var tipPercent = document.getElementById("").value;
+    var tipPercent = document.getElementById("buttons_form").value;
     var total = document.getElementById("total").value;
-
+        
     if (billAmt === "" || numOfPeople < 1) {
         alert("Can't be zero");
         return;
     }
     if (numOfPeople === "" || numOfPeople <=1){
         numberOfPeople = 1;
-
-    document.getElementById("each").style.display = "none";
-}else {
-    document.getElementById("each").style.display = "block";
-}
-
+    }
     var total = (billAmt * tipPercent) / numOfPeople;
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2);
@@ -24,9 +19,3 @@ function CalcTip() {
     document.getElementById("tip").innerHTML = total;
 }
 
-    document.getElementById("totalTip").style.display = "none";
-    document.getElementById("each").style.display = "none";
-
-    document.getElementById("calculate").onclick = function() {
-        calculateTip();
-}
