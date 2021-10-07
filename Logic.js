cbf89164893608a50887116aafe160252c5dc13e
Peter <phellwege1@gmail.com>
2021-10-06 18:40:35 -0600

    New Commit8

--- Logic.js
+++ Logic.js
@@ -1,36 +1,26 @@
-function getId(btn){
-        const DeezeNuts = (btn.id)
-}
-
 function CalcTip(billAmt, tipPercent, numOfPeople) {
-    var billAmt = document.getElementById("billMe").value;
-    var numOfPeople = document.getElementById("numberOfPeople").value;
-    var tipPercent = document.getElementById(`${DeezeNuts}``).value;
-
-    
-
-        console.log(billAmt)
-
-        console.log(numOfPeople)
-
-        console.log(tipPercent)
-
     if (billAmt === "" || numOfPeople < 1) {
         alert("Can't be zero");
         return;
     }
-
     var Total = (billAmt * tipPercent) / numOfPeople; 
     Total = Math.round(Total) + (billAmt / numOfPeople);
-    Total = Total.toFixed(2);
     Total = Total;
-    
-    console.log(billAmt* tipPercent)
-
     var Tip = (billAmt * tipPercent) / numOfPeople;
         Tip = Math.round(Tip);
-        Tip = Tip.toFixed(2);
-
-    document.getElementById("tip").innerHTML = Tip;
-    document.getElementById("total").innerHTML = Total;
-}
+    document.getElementById("tip").innerHTML = Tip.toFixed(2);
+    document.getElementById("total").innerHTML = Total.toFixed(2);
+}    
+    function getId(btn) {
+        const DeezeNuts = (btn.id)
+        var billAmt = document.getElementById("billMe").value;
+        var numOfPeople = document.getElementById("numberOfPeople").value;
+        var tipPercent = document.getElementById(DeezeNuts).value;
+        CalcTip(billAmt, tipPercent, numOfPeople)
+    }
+    function Reset() {
+        var x = document.getElementById("billMe")
+            x = "";
+        var y = document.getElementById("numOfPeople")
+            y="";
+    }
